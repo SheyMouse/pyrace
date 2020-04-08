b12ab8b4780e4ddf56d1d3844d4ae7d92a6d83ef
SheyMouse <sheymouse@gmail.com>
2020-04-08 01:00:44 +0100

    a bit more code

--- race1.py
+++ race1.py
@@ -3,3 +3,13 @@ import pgzrun
 
 WIDTH = 700 #Width of the game area
 HEIGHT = 800 # Height of the game area
+car = Actor("racecar") # Load in the actor image
+car.pos = 250, 700 # set the car position
+SPEED = 4
+trackCount = 0
+trackPosition = 250
+trackWidth = 120
+trackDirection = False
+trackLeft = [] # list of track barriers left
+trackRight = [] # list of track barriers right
+gameStatus = 0
